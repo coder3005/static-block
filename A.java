a59622f77a2ba7ed846ccde5f7ca550a59d8acec
coder3005 <138576028+coder3005@users.noreply.github.com>
2023-07-04 15:16:26 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,17 @@
+/* Static Block */
+class A 
+{
+    public static void main(String[] args) {
+        A r=new A();
+    }
+    A()
+    {
+       System.out.println("please share"); 
+    }
+    {
+        System.out.println("please like");
+    }
+    static{
+        System.out.println("learn coding");
+    }
+}
