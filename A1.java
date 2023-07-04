dbfdad7beeb34bb95f81a5b1848a0740f1ad5afb
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-04 17:21:00 +0530

    Create A1.java

---
+++ A1.java
@@ -0,0 +1,10 @@
+/* Static Block */
+class A 
+{
+    static{
+        System.out.println("learn coding");
+    }
+    public static void main(String[] args) {      /* if we don't write this main method then, it will give error */
+        
+    }
+}
